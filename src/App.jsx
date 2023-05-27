6df733cd107b6c99f24fc0d5e71853af019d676a
Emanuel Vidal <emanuelvidalrm@gmail.com>
2023-05-27 14:42:58 -0300

    added dynamic reloading of list after deleting element

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
+import React, {useState, useEffect} from 'react';
 import './App.css'
 import Lista from './components/Lista/index.jsx'
 
 function App() {
 
+  const [lista, setLista] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function handleDelete(id) {
+    const response = await fetch(`http://localhost:3000/produtos/${id}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    console.log(data);
+    alert(id, 'item deleted!')
+    setLista((prevLista) => prevLista.filter((item) => item.id !== id));
+  }
+
+  async function getData() {
+    const response = await fetch("http://localhost:3000/produtos");
+    const data = await response.json();
+    console.log('incoming data:', data);
+    setLista(data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <>
       <div className='bg-slate-500 h-screen w-screen items-center justify-center flex flex-col'>
-        <h1 className='font-bold text-white text-3xl mb-4'>Lista App v1.0 🚀</h1>
+        <h1 className='font-bold text-white text-3xl mb-4'>Lista App v1.0 🚀 <span className='ml-56'>+</span></h1>
         <div className='bg-white w-2/6 h-4/6 rounded-md border border-slate-100 drop-shadow-md flex flex-col items-center'>
           <div className='flex flex-col items-center justify-center'>
             <h1 className='text-center font-bold text-2xl mb-2'>Estoque</h1>
-            <hr className='w-4/6'/>
+            <hr className='w-4/6' />
           </div>
           <div className='bg-red-500 mt-4 w-5/6 h-5/6 flex flex-col'>
-            <Lista/>
+            <Lista loading={loading} handleDelete={(id) => handleDelete(id)} lista={lista} />
           </div>
         </div>
       </div>
--- src/components/Lista/index.jsx
+++ src/components/Lista/index.jsx
@@ -1,51 +1,20 @@
-import { useEffect, useState } from "react"
+import React from  'react';
 
-export default function List() {
+export default function List({loading, lista, handleDelete}) {
 
-    const [lista, setLista] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-
-
-    async function handleDelete(id){
-       const response = await fetch(`http://localhost:3000/produtos/${id}`, {
-        method: "DELETE",
-       });
-       const data =  await response.json;
-       console.log(data);
-       alert(id, 'item deleted!')
-    }
-
-
-    async function getData() {
-
-        const response = await fetch("http://localhost:3000/produtos");
-        const data = await response.json();
-        console.log('incoming data:', data);
-        lista.push(data);
-        console.log('data stored:', lista);
-        setLoading(false);
-
-    }
-
-    useEffect(() => {
-        getData()
-    }, [])
-
-
-    return (
+    return (        
         <>
             <div className="flex flex-col bg-green-500 items-center align-center justify-center w-full">
-                {loading ? (
+                {loading ? ( 
                     <h1>Carregando dados...</h1>
                 ):(<div>
-                    {lista[0].map((item, index) => {
+                    {lista.map((item, index) => {
                         return(
                             <div key={item.id} className="bg-blue-200 w-[390px] flex flex-row justify-between h-1/2 items-center">
-                                <p className="mr-12" key={item.id}>{item.id}</p>
-                                <p key={item.id}>{item.descricao}</p>
-                                <p key={item.id}>R${item.preco}</p>
-                                <button key={item.id}>X</button>
+                                <p className="mr-12">{item.id}</p>
+                                <p>{item.descricao}</p>
+                                <p>R${item.preco}</p>
+                                <button onClick={() => handleDelete(item.id)}>X</button>
                             </div>
                         )
                     })}
