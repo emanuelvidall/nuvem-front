5e47589c863b97753c11604194db42d5e0762e68
Emanuel Vidal <emanuelvidalrm@gmail.com>
2023-05-27 18:29:59 -0300

    minor adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Lista from './components/Lista/index.jsx';
 import Modal from './components/Modal/index.jsx';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquarePlus } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
@@ -47,7 +45,7 @@ function App() {
     <>
       <Modal modalOpen={modalOpen} handleModalOpen={handleModalOpen} getData={getData} />
       <div className='bg-slate-500 h-screen w-screen items-center justify-center flex flex-col'>
-        <h1 className='font-bold text-white text-3xl mb-4 w-[600px] justify-between flex flex-row'>Lista App v1.0 🚀 <button className='' onClick={handleModalOpen}><FontAwesomeIcon icon={faSquarePlus} color='#66fc03' /></button></h1>
+        <h1 className='font-bold text-white text-3xl mb-4 w-[600px] justify-between flex flex-row'>Lista App v1.0 🚀 <button className='' onClick={handleModalOpen}><img className='w-[50px] h-[50px] hover:scale-110 transition ease-in-out delay-150' src='./add.png' alt='addIcon'></img></button></h1>
         <div className='bg-white w-[600px] h-4/6 rounded-md border border-slate-100 drop-shadow-md flex flex-col items-center'>
           <div className='flex flex-col items-center justify-center'>
             <h1 className='text-center font-bold text-2xl mb-2'>Estoque</h1>
@@ -61,6 +59,12 @@ function App() {
             <Lista loading={loading} handleDelete={(id) => handleDelete(id)} lista={lista} />
           </div>
         </div>
+        <div className='text-white flex flex-row items-center mt-2 mb-2 justify-evenly w-[600px]'>
+          <a href='https://github.com/emanuelvidall' target='_blank'><h1>Emanuel Vidal</h1></a>
+          <a href='https://github.com/Fcsla' target='_blank'><h1>Fernando Custodio</h1></a>
+          <a href='https://github.com/rafaelsaboiad' target='_blank'><h1>Rafael Saboia</h1></a>
+        </div>
+        <a href='https://unifor.br' target='_blank'><h1 className='text-white'>UNIFOR 2023</h1></a>
       </div>
       <ToastContainer />
     </>
--- src/components/Lista/index.jsx
+++ src/components/Lista/index.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faCircleXmark} from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 export default function List({ loading, lista, handleDelete }) {
 
+    function capitalizeFirstLetter(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
     return (
+
         <>
             {loading ? (
                 <h1>Carregando dados...</h1>
             ) : (<div>
                 {lista.map((item, index) => {
                     return (
-                        <div key={item.id} className="bg-white border border-slate-200 w-[390px] flex flex-row justify-between h-[50px] ml-px rounded-md items-center mb-4 mt-4 animate__animated animate__fadeInUp cursor-pointer hover:drop-shadow-md">
+                        <div key={item.id} className="bg-white border border-slate-200 w-[390px] flex flex-row justify-between h-[50px] ml-px rounded-md items-center mb-4 mt-4 animate__animated animate__fadeInUp cursor-pointer hover:drop-shadow-md transition ease-in-out delay-150">
                             <p className="mr-12 ml-2 font-bold">{item.id}</p>
-                            <div className='text-left justify-left align-left w-[150px] absolute left-10'><p>{item.descricao}</p></div>
+                            <div className='text-left justify-left align-left w-[150px] absolute left-10'><p>{capitalizeFirstLetter(item.descricao)}</p></div>
                             <p className='text-right absolute right-10'>R$ {item.preco.toFixed(2)}</p>
-                            <button className='mr-2' onClick={() => handleDelete(item.id)}><FontAwesomeIcon icon={faCircleXmark} color='#fc0303' /></button>
+                            <button className='mr-2' onClick={() => handleDelete(item.id)}><img className='w-[25px] h-[25px] hover:scale-110 transition ease-in-out delay-150' src='./delete.png' alt='addIcon'></img></button>
                         </div>
                     )
                 })}
